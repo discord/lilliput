d25278aea62f3ff2f9e03349d7cc05c340e5678c
Brian Armstrong <brian.armstrong@discordapp.com>
2017-04-10 15:00:29 -0700

    more decode features, more bounds checking

--- opencv_giflib.cpp
+++ opencv_giflib.cpp
@@ -59,36 +59,121 @@ bool giflib_decoder_slurp(giflib_Decoder d) {
 
 bool giflib_decoder_decode(giflib_Decoder d, int frame_index, opencv_Mat mat) {
     cv::Mat *cvMat = static_cast<cv::Mat *>(mat);
+    GraphicsControlBlock FirstGCB;
+    DGifSavedExtensionToGCB(d->gif, 0, &FirstGCB);
+    int first_transparency_index = FirstGCB.TransparentColor;
+    uint8_t bg_red, bg_green, bg_blue, bg_alpha;
+    if (d->gif->SBackGroundColor == first_transparency_index) {
+        bg_red = bg_green = bg_blue = bg_alpha = 0;
+    } else {
+        bg_red = d->gif->SColorMap->Colors[d->gif->SBackGroundColor].Red;
+        bg_green = d->gif->SColorMap->Colors[d->gif->SBackGroundColor].Green;
+        bg_blue = d->gif->SColorMap->Colors[d->gif->SBackGroundColor].Blue;
+        bg_alpha = 255;
+    }
+
     GraphicsControlBlock GCB;
     DGifSavedExtensionToGCB(d->gif, frame_index, &GCB);
     int transparency_index = GCB.TransparentColor;
 
-    // XXX make sure d.Width == mat.Width && d.Height == mat.Height
+    SavedImage im = d->gif->SavedImages[frame_index];
 
-    int width = cvMat->cols;
-    int height = cvMat->rows;
+    int frame_left = im.ImageDesc.Left;
+    int frame_top = im.ImageDesc.Top;
+    int frame_width = im.ImageDesc.Width;
+    int frame_height = im.ImageDesc.Height;
+
+    int buf_width = cvMat->cols;
+    int buf_height = cvMat->rows;
+
+    if (frame_left < 0) {
+        return false;
+    }
+
+    if (frame_top < 0) {
+        return false;
+    }
+
+    if (frame_width < 0) {
+        return false;
+    }
+
+    if (frame_height < 0) {
+        return false;
+    }
+
+    if (frame_left + frame_width > buf_width) {
+        return false;
+    }
+
+    if (frame_top + frame_height > buf_height) {
+        return false;
+    }
 
     ColorMapObject *globalColorMap = d->gif->SColorMap;
-    ColorMapObject *frameColorMap = d->gif->SavedImages[frame_index].ImageDesc.ColorMap;
+    ColorMapObject *frameColorMap = im.ImageDesc.ColorMap;
     ColorMapObject *colorMap = frameColorMap ? frameColorMap : globalColorMap;
-    // XXX colorMap NULL
-    bool draw_background = (frame_index == 0);
+
+    if (!colorMap) {
+        return false;
+    }
+
+    if (frame_index == 0) {
+        // first frame -- draw the background
+        for (size_t y = 0; y < buf_height; y++) {
+            uint8_t *dst = cvMat->data + y * cvMat->step;
+            for (size_t x = 0; x < buf_width; x++) {
+                *dst++ = bg_blue;
+                *dst++ = bg_green;
+                *dst++ = bg_red;
+                *dst++ = bg_alpha;
+            }
+        }
+    }
+
+    if (frame_index > 0) {
+        int previous_frame_index = frame_index - 1;
+        GraphicsControlBlock prevGCB;
+        DGifSavedExtensionToGCB(d->gif, previous_frame_index, &prevGCB);
+        int prev_disposal = prevGCB.DisposalMode;
+        if (prev_disposal == DISPOSE_BACKGROUND) {
+            // draw over the previous frame with the BG color
+            // TODO should we do bounds checking here?
+            SavedImage prevIM = d->gif->SavedImages[previous_frame_index];
+            int prev_frame_left = prevIM.ImageDesc.Left;
+            int prev_frame_top = prevIM.ImageDesc.Top;
+            int prev_frame_width = prevIM.ImageDesc.Width;
+            int prev_frame_height = prevIM.ImageDesc.Height;
+            for (int y = prev_frame_top; y < prev_frame_top + prev_frame_left; y++) {
+                uint8_t *dst = cvMat->data + y * cvMat->step;
+                for (int x = prev_frame_left; x < prev_frame_left + prev_frame_width; x++) {
+                    *dst++ = bg_blue;
+                    *dst++ = bg_green;
+                    *dst++ = bg_red;
+                    *dst++ = bg_alpha;
+                }
+            }
+        } else if (prev_disposal == DISPOSE_PREVIOUS) {
+            // TODO or maybe not to do
+            // should we at least log this happened so that we know this exists?
+            // tldr this crazy method requires you to walk back across all previous
+            //    frames until you reach one with DISPOSAL_DO_NOT
+            //    and "undraw them", most likely would be done by building a temp
+            //    buffer when first one is encountered
+        }
+    }
+
+    // TODO handle interlaced gifs?
 
     int bit_index = 0;
-    for (int y = 0; y < height; y++) {
+    for (int y = frame_top; y < frame_top + frame_height; y++) {
         uint8_t *dst = cvMat->data + y * cvMat->step;
-        for (int x = 0; x < width; x++) {
-            GifByteType palette_index = d->gif->SavedImages[frame_index].RasterBits[bit_index++];
+        for (int x = frame_left; x < frame_left + frame_width; x++) {
+            GifByteType palette_index = im.RasterBits[bit_index++];
             if (palette_index == transparency_index) {
-                if (draw_background) {
-                    // TODO draw_background
-                    dst += 4;
-                    continue;
-                } else {
-                    // TODO: don't hardcode 4 channels (8UC4) here
-                    dst += 4;
-                    continue;
-                }
+                // TODO: don't hardcode 4 channels (8UC4) here
+                dst += 4;
+                continue;
             }
             *dst++ = colorMap->Colors[palette_index].Blue;
             *dst++ = colorMap->Colors[palette_index].Green;
