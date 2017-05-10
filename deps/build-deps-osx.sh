b4adb2453ec19c446d0a1dc270f1c8b20d48a107
Brian Armstrong <brian.armstrong@discordapp.com>
2017-05-09 17:53:48 -0700

    build libpng with --enable-intel-sse

--- deps/build-deps-osx.sh
+++ deps/build-deps-osx.sh
@@ -41,7 +41,7 @@ mkdir -p $BASEDIR/libpng
 tar -xzf $BASEDIR/libpng-1.6.29.tar.gz -C $BASEDIR/libpng --strip-components 1
 mkdir -p $BUILDDIR/libpng
 cd $BUILDDIR/libpng
-$BASEDIR/libpng/configure --prefix=$PREFIX --disable-shared --enable-static
+$BASEDIR/libpng/configure --prefix=$PREFIX --disable-shared --enable-static --enable-intel-sse
 make
 make install
 
