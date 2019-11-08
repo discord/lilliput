a97272cc1cc26b9036f4ac35cc314faeb34446f6
Nelly <kodie.goodwin+automation@discordapp.com>
2019-11-07 18:01:06 -0800

    Automated Deps update: 2019-11-08 01:52:42+00:00 (#64)

--- deps/linux/share/OpenCV/OpenCVModules-release.cmake
+++ deps/linux/share/OpenCV/OpenCVModules-release.cmake
@@ -9,7 +9,7 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(opencv_core PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
-  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "/home/brian_armstrong/lilliput/deps/linux/lib/libz.a;stdc++;dl;m;pthread;rt;ippicv"
+  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "/src/deps/linux/lib/libz.a;stdc++;dl;m;pthread;rt;ippicv"
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libopencv_core.a"
   )
 
@@ -31,7 +31,7 @@ list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgproc "${_IMPORT_PREFIX}/lib/libope
 set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(opencv_imgcodecs PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
-  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "opencv_core;opencv_imgproc;stdc++;dl;m;pthread;rt;ippicv;/home/brian_armstrong/lilliput/deps/linux/lib/libz.a;/home/brian_armstrong/lilliput/deps/linux/lib/libjpeg.a;/home/brian_armstrong/lilliput/deps/linux/lib/libwebp.a;/home/brian_armstrong/lilliput/deps/linux/lib/libpng.a;/home/brian_armstrong/lilliput/deps/linux/lib/libz.a"
+  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "opencv_core;opencv_imgproc;stdc++;dl;m;pthread;rt;ippicv;/src/deps/linux/lib/libz.a;/src/deps/linux/lib/libjpeg.a;/src/deps/linux/lib/libwebp.a;/src/deps/linux/lib/libpng.a;/src/deps/linux/lib/libz.a"
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libopencv_imgcodecs.a"
   )
 
