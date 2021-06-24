e27121e95baad3bef17a650b6ca05632f747369b
brian-armstrong-discord <brian.armstrong@discordapp.com>
2021-06-24 14:25:29 -0700

    don't use symlinks for macos build (#92)
    * don't use symlinks for macos build
    
    * Update deps/copy-symlink-target.sh
    
    Co-authored-by: Isobel Redelmeier <iredelmeier@gmail.com>
    
    Co-authored-by: Isobel Redelmeier <iredelmeier@gmail.com>

--- deps/copy-symlink-target.sh
+++ deps/copy-symlink-target.sh
@@ -4,7 +4,16 @@ set -eu
 
 main() {
   symlink="$1"
-  target="$(readlink -f "$symlink")"
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    if command -v greadlink > /dev/null; then
+      target="$(greadlink -f "$symlink")"
+    else
+      echo "Please install coreutils"
+      exit 1
+    fi
+  else
+    target="$(readlink -f "$symlink")"
+  fi
 
   rm "$symlink"
   cp "$target" "$symlink"
