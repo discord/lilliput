234741348725efd98b678f6ee533351c2c9d8451
Adam Jensen <acjensen@gmail.com>
2024-05-07 11:38:53 -0400

    Remove unused artifacts from deps

--- deps/build-deps-linux.sh
+++ deps/build-deps-linux.sh
@@ -126,6 +126,9 @@ rm -f $BASEDIR/linux/$ARCH/**/*.cmake
 # is that much more limited than bash.
 find "$PREFIX" -type l -exec "${BASEDIR}/copy-symlink-target.sh" {} \;
 
+rm -rf $BASEDIR/linux/$ARCH/bin
+rm -f $BASEDIR/linux/$ARCH/**/*.cmake
+
 if [ -n "$CI" ]; then
   echo "CI detected, cleaning up build artifacts"
   rm -rf "$SRCDIR"
