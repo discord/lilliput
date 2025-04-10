ec73918d78776366d503d4e00aef396eb3bdaf57
Scott Kidder <scott.kidder@discordapp.com>
2025-04-10 22:53:00 +0000

    misc: Update compiled deps to include lcms from 79f3a67.

--- deps/linux/aarch64/lib/cmake/opencv4/OpenCVModules-release.cmake
+++ deps/linux/aarch64/lib/cmake/opencv4/OpenCVModules-release.cmake
@@ -12,18 +12,8 @@ set_target_properties(libopenjp2 PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/liblibopenjp2.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS libopenjp2 )
-list(APPEND _IMPORT_CHECK_FILES_FOR_libopenjp2 "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/liblibopenjp2.a" )
-
-# Import target "libpng" for configuration "Release"
-set_property(TARGET libpng APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(libpng PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "ASM;C"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/liblibpng.a"
-  )
-
-list(APPEND _IMPORT_CHECK_TARGETS libpng )
-list(APPEND _IMPORT_CHECK_FILES_FOR_libpng "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/liblibpng.a" )
+list(APPEND _cmake_import_check_targets libopenjp2 )
+list(APPEND _cmake_import_check_files_for_libopenjp2 "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/liblibopenjp2.a" )
 
 # Import target "libprotobuf" for configuration "Release"
 set_property(TARGET libprotobuf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -32,8 +22,8 @@ set_target_properties(libprotobuf PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/liblibprotobuf.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS libprotobuf )
-list(APPEND _IMPORT_CHECK_FILES_FOR_libprotobuf "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/liblibprotobuf.a" )
+list(APPEND _cmake_import_check_targets libprotobuf )
+list(APPEND _cmake_import_check_files_for_libprotobuf "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/liblibprotobuf.a" )
 
 # Import target "ittnotify" for configuration "Release"
 set_property(TARGET ittnotify APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -42,8 +32,8 @@ set_target_properties(ittnotify PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/libittnotify.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS ittnotify )
-list(APPEND _IMPORT_CHECK_FILES_FOR_ittnotify "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/libittnotify.a" )
+list(APPEND _cmake_import_check_targets ittnotify )
+list(APPEND _cmake_import_check_files_for_ittnotify "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/libittnotify.a" )
 
 # Import target "ade" for configuration "Release"
 set_property(TARGET ade APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -52,8 +42,8 @@ set_target_properties(ade PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/libade.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS ade )
-list(APPEND _IMPORT_CHECK_FILES_FOR_ade "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/libade.a" )
+list(APPEND _cmake_import_check_targets ade )
+list(APPEND _cmake_import_check_files_for_ade "${_IMPORT_PREFIX}/lib/opencv4/3rdparty/libade.a" )
 
 # Import target "opencv_core" for configuration "Release"
 set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -62,8 +52,8 @@ set_target_properties(opencv_core PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libopencv_core.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS opencv_core )
-list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_core "${_IMPORT_PREFIX}/lib/libopencv_core.a" )
+list(APPEND _cmake_import_check_targets opencv_core )
+list(APPEND _cmake_import_check_files_for_opencv_core "${_IMPORT_PREFIX}/lib/libopencv_core.a" )
 
 # Import target "opencv_imgproc" for configuration "Release"
 set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -72,8 +62,8 @@ set_target_properties(opencv_imgproc PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libopencv_imgproc.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS opencv_imgproc )
-list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgproc "${_IMPORT_PREFIX}/lib/libopencv_imgproc.a" )
+list(APPEND _cmake_import_check_targets opencv_imgproc )
+list(APPEND _cmake_import_check_files_for_opencv_imgproc "${_IMPORT_PREFIX}/lib/libopencv_imgproc.a" )
 
 # Import target "opencv_photo" for configuration "Release"
 set_property(TARGET opencv_photo APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -82,8 +72,8 @@ set_target_properties(opencv_photo PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libopencv_photo.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS opencv_photo )
-list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_photo "${_IMPORT_PREFIX}/lib/libopencv_photo.a" )
+list(APPEND _cmake_import_check_targets opencv_photo )
+list(APPEND _cmake_import_check_files_for_opencv_photo "${_IMPORT_PREFIX}/lib/libopencv_photo.a" )
 
 # Import target "opencv_imgcodecs" for configuration "Release"
 set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -92,8 +82,8 @@ set_target_properties(opencv_imgcodecs PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libopencv_imgcodecs.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS opencv_imgcodecs )
-list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgcodecs "${_IMPORT_PREFIX}/lib/libopencv_imgcodecs.a" )
+list(APPEND _cmake_import_check_targets opencv_imgcodecs )
+list(APPEND _cmake_import_check_files_for_opencv_imgcodecs "${_IMPORT_PREFIX}/lib/libopencv_imgcodecs.a" )
 
 # Import target "opencv_highgui" for configuration "Release"
 set_property(TARGET opencv_highgui APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -102,8 +92,8 @@ set_target_properties(opencv_highgui PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libopencv_highgui.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS opencv_highgui )
-list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_highgui "${_IMPORT_PREFIX}/lib/libopencv_highgui.a" )
+list(APPEND _cmake_import_check_targets opencv_highgui )
+list(APPEND _cmake_import_check_files_for_opencv_highgui "${_IMPORT_PREFIX}/lib/libopencv_highgui.a" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
