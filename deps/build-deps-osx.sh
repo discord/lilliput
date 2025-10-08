3d04303430b9836e646eb87b1d57bb9855fa9fe6
Richard Kuo (Discord) <richard.kuo@discordapp.com>
2025-10-07 20:34:57 -0700

    bump libjpeg-turbo to 3.1.0

--- deps/build-deps-osx.sh
+++ deps/build-deps-osx.sh
@@ -75,7 +75,7 @@ echo '\n--------------------'
 echo 'Building libjpeg-turbo'
 echo '--------------------\n'
 mkdir -p $BASEDIR/libjpeg-turbo
-tar -xzf $SRCDIR/libjpeg-turbo-2.1.4.tar.gz -C $BASEDIR/libjpeg-turbo --strip-components 1
+tar -xzf $SRCDIR/libjpeg-turbo-3.1.0.tar.gz -C $BASEDIR/libjpeg-turbo --strip-components 1
 cd $BASEDIR/libjpeg-turbo
 mkdir -p $BUILDDIR/libjpeg-turbo
 cd $BUILDDIR/libjpeg-turbo
